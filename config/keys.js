ebc7d4f253e1891fe974e0e45f1e1d991f9b9d55
Anupam-Das1 <79990459+Anupam-Das1@users.noreply.github.com>
2021-07-12 11:03:36 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -3,5 +3,3 @@ if(process.env.NODE_ENV==='production'){
 }else{
     module.exports=require('./dev')
 }
-// "mongodb+srv://AnupamDas:Anupam@433454@cluster0.xxg6e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-// "abcdef"
